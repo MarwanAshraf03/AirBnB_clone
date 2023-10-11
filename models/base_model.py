1aa1514b3ef2ce890f2b9354aa8800d072be4f5b
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-10-11 10:37:49 +0300

    configure pycodestyle and putting comments

--- models/base_model.py
+++ models/base_model.py
@@ -8,22 +8,23 @@ from models import storage
 class BaseModel:
     """BaseModel class"""
 
-
     id = ""
     created_at = ""
     updated_at = ""
 
     def __init__(self, *args, **kwargs):
-        """
-        initializes Base class with id, time of creation, updating time
-        """
+        """initializes Base class with id, time of creation, updating time"""
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
                 setattr(self, key, value)
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(
+                        self,
+                        key,
+                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        )
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
@@ -31,22 +32,16 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """
-        converts class to string
-        """
+        """returns string representation of class"""
         return (f"[{__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        """
-        sets the updating time to now
-        """
+        """sets the updating time to now"""
         storage.save()
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        returns the dictionary representation of class
-        """
+        """returns the dictionary representation of class"""
         ret = self.__dict__
         ret['__class__'] = __class__.__name__
         ret['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
+"""
+file_storage Module
+"""
 import json
 from os import path
 
 
 class FileStorage:
+    """
+    FileStorage class
+    """
     __file_path = "file.json"
     __objects = {}
+
     def __init__(self):
+        """init method doesn't do anything"""
         pass
 
     def all(self):
+        """returns all the instances of the past"""
         return self.__objects
 
     def new(self, obj):
+        """adds a new object to the __object dictionary"""
         self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj.to_dict()
 
     def save(self):
+        """saves (serializes) __object dictionary to file.json"""
         with open(self.__file_path, "w", encoding='utf-8') as f:
             json.dump(self.__objects, f)
 
     def reload(self):
-        """Loads instances from json file"""
+        """Loads (deserializes) __object dictionary from file.json"""
         if not path.isfile(self.__file_path):
             return
         with open(self.__file_path, "r", encoding="utf-8") as f:

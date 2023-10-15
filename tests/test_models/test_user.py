342fae743b8abb9a223154a91fe67a880b252dd7
msudany <mssodany2002@gmail.com>
2023-10-15 22:09:09 +0300

    update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,1 +1,47 @@
 #!/usr/bin/python3
+''' '''
+
+import os
+import unittest
+from models.base_model import BaseModel
+from models.user import User
+
+class test_user(unittest.TestCase):
+    '''
+    
+    '''
+
+    def __init__(self, *args, **kwargs):
+        ''' '''
+        super().__init__(*args, **kwargs)
+        self.name = 'User'
+        self.cls = User
+
+    def test_instance_type(self):
+        ''' '''
+        my_user = User()
+        self.assertIsInstance(my_user, BaseModel)
+
+    def test_first_name(self):
+        ''' '''
+        my_user = User()
+        self.assertIsInstance(my_user.first_name, str)
+
+    def test_last_name(self):
+        ''' '''
+        my_user = User()
+        self.assertIsInstance(my_user.last_name, str)
+
+    def test_password(self):
+        ''' '''
+        my_user = User()
+        self.assertIsInstance(my_user.password, str)
+
+    def test_email(self):
+        ''' '''
+        my_user = User()
+        self.assertIsInstance(my_user.email, str)
+
+if __name__ == "__main__":
+    unittest.main()
+

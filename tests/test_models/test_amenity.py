49944cf238b663238c449b98827225c0f63d0f55
msudany <mssodany2002@gmail.com>
2023-10-15 21:47:26 +0300

    update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+''' '''
+
+import os
+import unittest
+from models.base_model import BaseModel
+from models.amenity import Amenity
+
+class test_Amenity(unittest.TestCase):
+    '''
+    
+    '''
+
+    def __init__(self, *args, **kwargs):
+        ''' '''
+        super().__init__(*args, **kwargs)
+        self.name = 'Amenity'
+        self.cls = Amenity
+
+    def test_instance_type(self):
+        ''' '''
+        my_amenity = Amenity()
+        self.assertIsInstance(my_amenity, BaseModel)
+
+    def test_name(self):
+        ''' '''
+        my_amenity = Amenity()
+        self.assertIsInstance(my_amenity.name, str)
+
+if __name__ == "__main__":
+    unittest.main()
+

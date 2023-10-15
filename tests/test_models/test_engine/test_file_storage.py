62f4db72eeb9a9f4b35c134275d78af275c42ca4
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2023-10-15 21:51:05 +0300

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -10,6 +10,8 @@ from models.city import City
 from models.review import Review
 from models.engine.file_storage import FileStorage
 from models import storage
+from os import path
+from os import remove
 
 
 class test_file_storage(unittest.TestCase):
@@ -17,13 +19,17 @@ class test_file_storage(unittest.TestCase):
     def test_type(self):
         self.assertIsInstance(storage, FileStorage)
 
+    def test_init(self):
+        temp = FileStorage()
+        self.assertEqual(type(temp).__name__, "FileStorage")
+
     def test_file_path(self):
-        st = FileStorage()
         self.assertTrue(hasattr(FileStorage, "_FileStorage__file_path"))
 
     def test_save_base_model(self):
         b = BaseModel()
         self.assertEqual(storage.all()[f"BaseModel.{b.id}"].id, b.id)
+
     def test_save_base_model(self):
         u = User()
         self.assertEqual(storage.all()[f"User.{u.id}"].id, u.id)
@@ -48,242 +54,17 @@ class test_file_storage(unittest.TestCase):
         r = Review()
         self.assertEqual(storage.all()[f"Review.{r.id}"].id, r.id)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def storage_reset(self):
+        if path.isfile("file.json"):
+            remove("file.json")
+
+    def test_save(self):
+        b = BaseModel()
+        dictionary = b.to_dict()
+        b1 = BaseModel(**dictionary)
+        b1.save()
+        self.assertNotEqual(str(storage.all()[f"BaseModel.{b1.id}"]), str(b1))
+        self.storage_reset()
 
 
 

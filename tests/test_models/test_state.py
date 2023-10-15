02948836df8def7266cbd19ba16f425c1ca20d76
msudany <mssodany2002@gmail.com>
2023-10-15 22:08:37 +0300

    update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+''' '''
+
+import os
+import unittest
+from models.base_model import BaseModel
+from models.state import State
+
+class test_state(unittest.TestCase):
+    '''
+    
+    '''
+
+    def __init__(self, *args, **kwargs):
+        ''' '''
+        super().__init__(*args, **kwargs)
+        self.name = 'State'
+        self.cls = State
+
+    def test_instance_inheritance(self):
+        ''' '''
+        my_state = State()
+        self.assertIsInstance(my_state, BaseModel)
+
+    def test_instance_type(self):
+        ''' '''
+        my_state = State()
+        self.assertIsInstance(my_state, self.cls)
+
+    def test_name(self):
+        ''' '''
+        my_state = State()
+        self.assertIsInstance(my_state.name, str)
+
+if __name__ == "__main__":
+    unittest.main()
+

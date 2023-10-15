91ef402092a7ae331bf1ad85fe941005198aba80
msudany <mssodany2002@gmail.com>
2023-10-15 23:27:04 +0300

    update test_file_storage.py

--- tests/test_models/engine/test_file_storage.py
+++ tests/test_models/engine/test_file_storage.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+'''Unittests for file storage'''
+
+import unittest
+from models.base_model import BaseModel
+from models import FileStorage
+import json
+import datetime
+
+
+class TestFileStorage(unittest.TestCase):
+    ''' '''
+
+    def test_instance_type(self):
+        ''' '''
+        storage = FileStorage()
+        self.assertIsInstance(storage, FileStorage)        
+
+    def test_all(self):
+        ''' '''
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
